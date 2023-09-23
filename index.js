1af800c6b48ed88f038ecc61fec6c5d4663113e2
gleeebkaaa <g.pudovkin1996@gmail.com>
2023-09-23 10:25:12 +0300

    add lodash and prettier

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-import _ from 'lodash';
+import _ from "lodash";
 
-console.log(_.last(['one', 'two']));
+console.log(_.last(["one", "two"]));
+
+const faHex = (a, b) => {
+  const result = a + b;
+  return result;
+};
